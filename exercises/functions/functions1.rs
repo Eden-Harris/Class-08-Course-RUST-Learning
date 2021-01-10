15702a091ab2e12659a9a9eaae14940a7b44fe75
Liao-Eden <oncwnuHp4q7ZyBxorwWcAuaF4Dvs@git.weixin.qq.com>
2021-01-10 21:51:52 +0800

    Homework completed on 01-10

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,12 +1,10 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
-
 fn call_me(value:i32)->i32{
     value
 }
 
 fn main() {
-    call_me();
+    call_me(12);
 }
